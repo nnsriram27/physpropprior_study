e94320f6b879d3e5e3e4fcc96b61bc6649a3e150
Sriram Nochur Narayanan <sriram@nec-labs.com>
2025-11-08 19:31:17 -0500

    initial commit

---
+++ upload_s3.sh
@@ -0,0 +1,6 @@
+export AWS_ACCESS_KEY_ID=AKIA2VIPRQW6PQGQLKXW
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_PROFILE=default
+export AWS_REGION=us-east-2
+
+s5cmd sync "./*" s3://sriram-misc/physpropprior_study/
